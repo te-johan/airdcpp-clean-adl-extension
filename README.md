6ab90f25aca813d6b31395f919df652105ce5988
maksis <maksis@adrenaline-network.com>
2017-04-18 19:23:30 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# airdcpp-example-python-extension
+
+Example extension for AirDC++ written in Python 3.x. The extension will spam a message to event log once per minute. No external dependencies are required.
+
+## Installation
+
+Click *Install from URL* from the extension management page and paste the URL of the [latest release](https://github.com/airdcpp-web/airdcpp-example-python-extension/releases) (.tar.gz format only).
---
+++ package/extension.py
@@ -0,0 +1,38 @@
+import argparse
+import json
+import threading
+import time
+
+import urllib.request
+
+
+# Spam a test message to event log once per minute
+
+# Arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--apiUrl", help="API URL")
+parser.add_argument("--name", help="Extension name")
+parser.add_argument("--authToken", help="API session token")
+parser.add_argument("--settingsPath", help="Setting directory")
+parser.add_argument("--logPath", help="Log directory")
+
+args = parser.parse_args()
+
+
+# Initialize API request
+message = {
+	'text': 'Test message sent by extension {}'.format(args.name),
+	'severity': 'info'
+}
+
+postRequest = urllib.request.Request('http://' + args.apiUrl + 'events')
+postRequest.add_header('Authorization', args.authToken)
+postRequest.add_header('Content-Type', 'application/json')
+
+# Sending
+def sendmessage():
+	threading.Timer(60.0, sendmessage).start()
+	response = urllib.request.urlopen(postRequest, json.dumps(message).encode('utf-8'))
+
+sendmessage()
